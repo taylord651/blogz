50e520e4edb176c0198ea3bd738a22ad2630fdff
Domonique Taylor <taylord651@gmail.com>
2019-07-14 20:34:04 -0500

    All blog post are displayed on main page, users are redirected to the 'newpost.html' page to submit new blog posts

--- main.py
+++ main.py
@@ -11,30 +11,41 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     content = db.Column(db.String(500))
+    author = db.Column(db.String(120))
+    date = db.Column(db.String(120))
 
-    def __init__(self, title, content):
+    def __init__(self, title, author, date, content):
         self.title = title
+        self.author = author
+        self.date = date
         self.content = content
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
 
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        blog_post = Blog(title, content)
-        db.session.add(blog_post)
-        db.session.commit()
-
     titles = Blog.query.all()
+    authors = Blog.query.all()
+    dates = Blog.query.all()
     contents = Blog.query.all()
 
-    return render_template("index.html", title="Build A Blog", 
-        titles=titles, contents=contents)
+    return render_template("blog.html", title="Build A Blog", 
+        titles=titles, contents=contents,
+        authors=authors, dates=dates)
 
-@app.route("/form")
-def submit_form():
-    return render_template("form.html")
+@app.route("/newpost", methods=['POST', 'GET'])
+def submit_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        date = request.form['date']
+        content = request.form['content']
+        
+        blog_post = Blog(title, author, date, content)
+        db.session.add(blog_post)
+        db.session.commit()
+        return redirect ('/')
+
+    return render_template("newpost.html")
 
 @app.route("/display")
 def display_form():
